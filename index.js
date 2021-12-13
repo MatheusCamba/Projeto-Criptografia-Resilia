e64c1b00ae036cb430272c1883a96a34519999f1
Matheus Camba <mhscamba@gmail.com>
2021-12-13 17:06:30 -0300

    COD/DECOD Cifra de César incluído, e base64 com mudança. Funcionamento perfeito

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var btn = document.getElementById("btnEnviar")
 
 var textoDaArea = document.getElementById("textArea")
 
-var alfabeto = "a"
+var alfabeto = "abcdefghijklmnopqrstuvwxyzéáàèâêçìíòó"
 
 //EVENTO INCREMENTO - CONCLUIDO
 select.addEventListener("change", function(){
@@ -26,7 +26,7 @@ select.addEventListener("change", function(){
 })
 //EVENTO INCREMENTO - CONCLUIDO
 
-//EVENTO BOTAO - CONCLUIDO
+//EVENTO BOTAO COD E DECOD - CONCLUIDO
 radioCodificar.addEventListener("click", function(){
     btn.innerText = "Codificar Mensagem!"
 })
@@ -34,28 +34,83 @@ radioCodificar.addEventListener("click", function(){
 radioDecodificar.addEventListener("click", function(){
     btn.innerText = "Decodificar Mensagem!"
 })
-//EVENTO BOTAO - CONCLUIDO
+//EVENTO BOTAO COD E DECOD - CONCLUIDO
 
-//EVENTO CODIFICAR E DECODIFICAR O BASE64 - CONCLUIDO
-btn.addEventListener("click", function(e){
-   e.preventDefault();
+//FUNÇÃO CODIFICAR CIFRA DE CÉSAR
+function codificaCesar() {
+    var textoDigitado= texto.value;
+    var textoMinusculo= textoDigitado.toLowerCase();
+    var textoCodificadoCifra= '';
+ 
+    for (var i= 0; i < textoMinusculo.length; i++) {
+       for (var j= 0; j < alfabeto.length; j++) {
+          if (textoMinusculo [i] == alfabeto [j]) {
+             textoCodificadoCifra += alfabeto[j + parseInt(incremento.value)]
+             break
+          } else if (textoMinusculo == ' ') {
+             textoCodificadoCifra=+ ' ';
+             break
+          }
+       
+       }
+    }
+ 
+    return textoCodificadoCifra
+ }
+
+ //FUNÇÃO DECODIFICAR CIFRA DE CÉSAR
+ function decodificaCesar() {
+    var textoDigitado= texto.value;
+    var textoMinusculo= textoDigitado.toLowerCase();
+    var textoCodificadoCifra= '';
+ 
+    for (var i= 0; i < textoMinusculo.length; i++) {
+        for (var j= alfabeto.length; j >= 0; j--) {
+           if (textoMinusculo [i] == alfabeto [j]) {
+              textoCodificadoCifra += alfabeto[j - parseInt(incremento.value)]
+              break
+           } else if (textoMinusculo== ' ') {
+              textoCodificadoCifra =+ ' ';
+              break
+           }
+        
+        }
+     }
+     return textoCodificadoCifra
+  }
+  
+//FUNÇÕES COD/DECOD BASE64
+function codBase64 (){
+    let texto = document.querySelector('#texto').value
+    let codificado = btoa(texto)
+    return codificado
+}
+
+function deCodBase64 (){
+    let texto = document.querySelector('#texto').value
+    let decodificado = atob(texto)
+    return decodificado
+}
+
+
+//EVENTO BOTAO PARA COD/DECOD CIFRA/BASE64
+
+btn.addEventListener('click', function(e){
+    e.preventDefault();
     if(radioCodificar.checked && select.value === "2"){
         
-        var textoAplicado = document.getElementById("texto").value
-        var textoCodificado = btoa(textoAplicado)
-        textoDaArea.innerHTML = `${textoCodificado}`
-    } 
-    }
-  
-)
-
-btn.addEventListener("click", function(e){
-    e.preventDefault();
-    if(radioDecodificar.checked && select.value === "2"){
-        var textoAplicado = document.getElementById("texto").value
-        var textoDecodificado = atob(textoAplicado)
-        textoDaArea.innerHTML = `${textoDecodificado}`
+        textoDaArea.value = codBase64();
+    } else if(radioDecodificar.checked && select.value === "2"){
+        
+        textoDaArea.value = deCodBase64();
+    } else if(radioCodificar.checked && select.value === "1"){
+        
+        textoDaArea.value = codificaCesar()
+    } else if(radioDecodificar.checked && select.value === "1"){
+        
+        textoDaArea.value = decodificaCesar()
+    } else{
+        textoDaArea.value = "Digite novamente o texto."
     }
 })
-//EVENTO CODIFICAR E DECODIFICAR O BASE64 - CONCLUIDO
 
